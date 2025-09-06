c9148f1867ebca5ae5eba3c27449d76489a1f3dc
CristalMell <2a9gon@gmail.com>
2025-09-06 12:43:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace laba1_Selivanov_ISP233
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello,World!!!");
+            //int number = 10;
+            //Console.WriteLine(number);//ctrl + k + c
+            //Console.Write("Pls name ");
+            //string? name = Console.ReadLine();
+            //Console.WriteLine($"You name: {name}");
+            //Console.Write("Pls name ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Pls age ");
+            //string? age = Console.ReadLine();
+            //Console.Write("Pls city ");
+            //string? city = Console.ReadLine();
+            //Console.WriteLine("Name: {0} age:{1} city:{2}", name, age, city);
+            //Console.Write("You num:");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine(age);
+            //int age2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(age2);
+
+            int number = 250;
+            long number2 = (byte)(number+10);
+            Console.WriteLine(number2);
+
+            bool alive = true;
+            bool isDead = false;
+
+            double iDouble = 4.5;
+            float iFloat = 3.5F;
+            decimal iDecimal = 2.5m;
+
+            var name = "Dfs";
+            char symbol = 'A';
+        }
+    }
+}
